16d8fee151bae2abb9f6f4ca8efa51da32051489
Matus Tejiscak <ziman@functor.sk>
2020-10-24 23:21:43 +0200

    Better protocol.

--- backend/src/Main.hs
+++ backend/src/Main.hs
@@ -6,11 +6,13 @@ import Data.Functor ((<&>))
 import Data.Char (isPunctuation, isSpace)
 import Data.Monoid (mappend)
 import Data.Text (Text)
+import Data.List (stripPrefix)
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
 import qualified Data.ByteString as BS
 
-import Data.Aeson
+import Data.Aeson.Casing
+import qualified Data.Aeson as Aeson
 
 import Control.Monad (forever)
 import Control.Monad.IO.Class (liftIO)
@@ -35,44 +37,74 @@ data Env = Env
   , envState :: TVar State
   }
 
+data Error
+  = ClientError Text  -- keep the connection
+  | GeneralError Text  -- kill the connection
+  deriving (Eq, Ord, Show, Generic)
+
+jsonOptions :: Aeson.Options
+jsonOptions = (aesonPrefix camelCase)
+  { Aeson.sumEncoding = Aeson.defaultTaggedObject
+  }
+
 data Message_C2S
   = Join
     { mcsUserName :: Text
     }
   deriving (Eq, Ord, Show, Generic)
 
-instance FromJSON Message_C2S
+instance Aeson.FromJSON Message_C2S where
+  parseJSON = Aeson.genericParseJSON jsonOptions
 
 data Message_S2C
   = Update
     { mscClients :: [Text]
     }
+  | Error
+    { mscMessage :: Text
+    }
   deriving (Eq, Ord, Show, Generic)
 
-instance ToJSON Message_S2C
+instance Aeson.ToJSON Message_S2C where
+  toJSON = Aeson.genericToJSON jsonOptions
 
-type Api = ReaderT Env (ExceptT String IO)
+type Api = ReaderT Env (ExceptT Error IO)
 
-throw :: String -> Api a
+throw :: Error -> Api a
 throw = lift . throwE
+
+throwClient :: String -> Api a
+throwClient = throw . ClientError . Text.pack
+
+throwGeneral :: String -> Api a
+throwGeneral = throw . GeneralError . Text.pack
 
 recv :: Api Message_C2S
 recv = do
   conn <- envConnection <$> ask
   msgBS <- liftIO $ WS.receiveData conn
-  case decode msgBS of
-    Nothing -> throw $ "can't decode: " ++ show msgBS
+  case Aeson.decode msgBS of
+    Nothing -> throwGeneral $ "can't decode: " ++ show msgBS
     Just msg -> pure msg
 
 send :: Message_S2C -> Api ()
 send msg = do
   conn <- envConnection <$> ask
-  liftIO $ WS.sendTextData conn (encode msg)
+  liftIO $ WS.sendTextData conn (Aeson.encode msg)
+
+loop :: Api () -> Api a
+loop api = do
+  liftCatch catchE api $ \case
+    ClientError msg -> do
+      send Error{ mscMessage = msg }
+      loop api
+    GeneralError msg -> throw $ GeneralError msg
+  loop api
 
 clientLoop :: Api ()
 clientLoop = do
   send $ Update{ mscClients = ["hello", "world"] }
-  forever $
+  loop $
     recv >>= \case
       Join{..} -> do
         liftIO $ print mcsUserName
@@ -87,7 +119,7 @@ application tvState pending = do
           , envState      = tvState
           }
     runExceptT (runReaderT clientLoop env) >>= \case
-      Left err -> putStrLn $ "error: " ++ err
+      Left err -> putStrLn $ "error: " ++ show err
       Right () -> pure ()
 
 main :: IO ()
