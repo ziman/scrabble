107ce76058d7672466619b3e0c549f45d9c92737
Matus Tejiscak <ziman@functor.sk>
2020-10-24 15:38:15 +0200

    Add some code.

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,40 @@
 module Main where
 
+import Data.Functor ((<&>))
+import Data.Char (isPunctuation, isSpace)
+import Data.Monoid (mappend)
+import Data.Text (Text)
+import Control.Exception (finally)
+import Control.Monad (forM_, forever)
+import Control.Concurrent (MVar, newMVar, modifyMVar_, modifyMVar, readMVar)
+import qualified Data.Text as Text
+import qualified Data.Text.IO as Text
+
+import qualified Network.WebSockets as WS
+
+data Client = Client
+  { clName :: Text
+  , clConnection :: WS.Connection
+  }
+
+data State = State
+  { stClients :: [Client]
+  }
+
+application :: MVar State -> WS.ServerApp
+application mvState pending = do
+  conn <- WS.acceptRequest pending
+  WS.withPingThread conn 30 (return ()) $ do
+    msg <- WS.receiveData conn
+    _clients <- readMVar mvState <&> stClients
+    WS.sendTextData conn (msg :: Text)
+
 main :: IO ()
-main = putStrLn "hello world!"
+main = do
+  mvState <- newMVar initialState
+  WS.runServer "127.0.0.1" 9160
+    $ application mvState
+  where
+    initialState = State
+      { stClients = []
+      }
