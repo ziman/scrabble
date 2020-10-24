0fc9180bd67963244dee061ffef44908b63d4ca3
Matus Tejiscak <ziman@functor.sk>
2020-10-24 22:44:09 +0200

    Implement a simple protocol.

--- backend/src/Main.hs
+++ backend/src/Main.hs
@@ -1,18 +1,23 @@
 module Main where
 
+import GHC.Generics
+
 import Data.Functor ((<&>))
 import Data.Char (isPunctuation, isSpace)
 import Data.Monoid (mappend)
 import Data.Text (Text)
-import Control.Exception (finally)
-import Control.Monad (forM_, forever)
-import Control.Concurrent.STM
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
 import qualified Data.ByteString as BS
 
+import Data.Aeson
+
+import Control.Monad (forever)
+import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Reader
 import Control.Monad.Trans.Except
+import Control.Monad.Trans.Class
+import Control.Concurrent.STM
 
 import qualified Network.WebSockets as WS
 
@@ -30,16 +35,48 @@ data Env = Env
   , envState :: TVar State
   }
 
+data Message_C2S
+  = Join
+    { mcsUserName :: Text
+    }
+  deriving (Eq, Ord, Show, Generic)
+
+instance FromJSON Message_C2S
+
+data Message_S2C
+  = Update
+    { mscClients :: [Text]
+    }
+  deriving (Eq, Ord, Show, Generic)
+
+instance ToJSON Message_S2C
+
 type Api = ReaderT Env (ExceptT String IO)
+
+throw :: String -> Api a
+throw = lift . throwE
+
+recv :: Api Message_C2S
+recv = do
+  conn <- envConnection <$> ask
+  msgBS <- liftIO $ WS.receiveData conn
+  case decode msgBS of
+    Nothing -> throw $ "can't decode: " ++ show msgBS
+    Just msg -> pure msg
+
+send :: Message_S2C -> Api ()
+send msg = do
+  conn <- envConnection <$> ask
+  liftIO $ WS.sendTextData conn (encode msg)
 
 clientLoop :: Api ()
 clientLoop = do
-  pure ()
-  {-
-    msg <- WS.receiveData conn
-    _clients <- readMVar mvState <&> stClients
-    WS.sendTextData conn (msg :: Text)
-  -}
+  send $ Update{ mscClients = ["hello", "world"] }
+  forever $
+    recv >>= \case
+      Join{..} -> do
+        liftIO $ print mcsUserName
+        send $ Update{ mscClients = ["woo", "boo"] }
 
 application :: TVar State -> WS.ServerApp
 application tvState pending = do
