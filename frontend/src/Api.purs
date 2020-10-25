ea804e4c62dba657b3b383bf4497fe7ffb0a09df
Matus Tejiscak <ziman@functor.sk>
2020-10-25 20:18:39 +0100

    Add more JSON instances.

--- frontend/src/Api.purs
+++ frontend/src/Api.purs
@@ -13,13 +13,57 @@ type Letter =
   { letter :: String
   , value :: Int
   }
+
 data Boost = DoubleLetter | TripleLetter | DoubleWord | TripleWord
+
+instance boostDecodeJson :: DecodeJson Boost where
+  decodeJson json = do
+    obj <- decodeJson json
+    obj .: "tag" >>= case _ of
+      "DoubleLetter" -> pure DoubleLetter
+      "TripleLetter" -> pure TripleLetter
+      "DoubleWord" -> pure DoubleWord
+      "TripleWord" -> pure TripleWord
+      tag -> Left $ AtKey "tag" $ UnexpectedValue (fromString tag)
+
 data Cell = Blank | Boost Boost | Letter Letter
+
+instance cellDecodeJson :: DecodeJson Cell where
+  decodeJson json = do
+    obj <- decodeJson json
+    obj .: "tag" >>= case _ of
+      "Blank" -> pure Blank
+      "Boost" -> Boost <$> decodeJson json
+      "Letter" -> Letter <$> decodeJson json
+      tag -> Left $ AtKey "tag" $ UnexpectedValue (fromString tag)
+
 type Cookie = String
 
+type Board =
+  { cols :: Int
+  , rows :: Int
+  , cells :: Array (Array Cell)
+  }
+
+data Phase
+  = WaitingForPlayers
+
+instance phaseDecodeJson :: DecodeJson Phase where
+  decodeJson json = do
+    obj <- decodeJson json
+    obj .: "tag" >>= case _ of
+      "WaitingForPlayers" -> pure WaitingForPlayers
+      tag -> Left $ AtKey "tag" $ UnexpectedValue (fromString tag)
+
+type State =
+  { players :: Array String
+  , board :: Board
+  , letters :: Array Letter
+  }
+
 data Message_S2C
-  = Update { clients :: Array String }
-  | Error { message :: String }
+  = Error { message :: String }
+  | Update State
 
 instance msg_s2c_DecodeJson :: DecodeJson Message_S2C where
   decodeJson json = do
@@ -33,9 +77,10 @@ data Message_C2S
   = Join { playerName :: String }
 
 instance msg_c2s_EncodeJson :: EncodeJson Message_C2S where
-  encodeJson (Join obj) = "Join" ~> obj
+  encodeJson = case _ of
+    Join obj -> "Join" // obj
 
-infix 3 addTag as ~>
+infix 3 addTag as //
 addTag :: forall a. EncodeJson a => String -> a -> Json
 addTag t obj = caseJsonObject json f json
   where
